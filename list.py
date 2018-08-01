53d40c6a037555fccde0159f733abb34ee051fde
tianwei0905 <tianwei@163.com>
2018-08-01 21:36:34 +0800

    list

---
+++ list.py
@@ -0,0 +1,16 @@
+#!/usr/bin python3
+
+course = ['PHP', 'C++', 'Vim', 'Python', 'Linux', 'Ruby', 'BigData', 'Cloud']
+
+#course.sort()
+
+#print(course)
+
+#course.insert(1,'java')
+
+#print(course)
+
+course.pop(5)
+
+for out in course:
+	print(out)
